5d708bc251afae4cf41abfceaa4cddea4dca87bf
Fun Noober <71403430+FunNoober@users.noreply.github.com>
2023-07-12 00:28:33 -0400

    Delete adverts directory

--- adverts/client.lua
+++
@@ -1,3 +0,0 @@
-TriggerEvent("chat:addSuggestion", '/ad', "Send out an ad", {
-    { name="contents", help="When using multiple words, surround them in quotes"}
-})
--- adverts/server.lua
+++
@@ -1,9 +0,0 @@
-RegisterCommand("ad", function (source, args, rawCommand)
-    TriggerClientEvent("chat:addMessage", -1, {
-        args = {
-            "[AD]",
-            args[1],
-        },
-        color = {0, 255, 0}
-    })
-end, false)
