de9cb416ce30b9f9285db100e809a4acae652ebf
Fun Noober <71403430+FunNoober@users.noreply.github.com>
2023-07-12 18:14:17 -0400

    Delete adverts directory

--- adverts/client.lua
+++
@@ -1,9 +0,0 @@
-for i, v in ipairs(Config.agencies) do
-    local vL = string.lower(v)
-    TriggerEvent("chat:addSuggestion", "/" .. vL, "Send out an ad from " .. v, {
-        { name="contents", help="When using multiple words, surround them in quotes"}
-    })
-    TriggerEvent("chat:addSuggestion", "/" .. v, "Send out an ad from " .. v, {
-        { name="contents", help="When using multiple words, surround them in quotes"}
-    })
-end
--- adverts/config.lua
+++
@@ -1,4 +0,0 @@
-Config = {}
-
-Config.agencies = {"24/7", "RonGas", "LTD", "NOoSE", "FIB", "Families", "Vagos", "Ballas", "LSPD", "LSSD", "BCSO", "SAHP"}
-Config.acePerms = true
